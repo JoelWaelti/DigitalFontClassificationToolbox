c5996a1f0a4213ff6261fb9f650cf1cadd42d66e
Joel Wälti <joel.t.waelti@hotmail.com>
2024-05-08 10:16:26 +0200

    add visualization for thickness lines

--- src/main/java/ch/fhnw/ip5/digitalfontclassification/demos/ThicknessAlongPathVisualization.java
+++ src/main/java/ch/fhnw/ip5/digitalfontclassification/demos/ThicknessAlongPathVisualization.java
@@ -1,42 +1,60 @@
 package ch.fhnw.ip5.digitalfontclassification.demos;
 
+import ch.fhnw.ip5.digitalfontclassification.analysis.LineThicknessAnalyzer;
 import ch.fhnw.ip5.digitalfontclassification.domain.*;
 import ch.fhnw.ip5.digitalfontclassification.domain.Point;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static ch.fhnw.ip5.digitalfontclassification.analysis.LineThicknessAnalyzer.computeThicknessAlongPathAtMiddleOfSegments;
 
-public class FlattenedGlyphView extends JPanel {
+public class ThicknessAlongPathVisualization extends JPanel {
     private Glyph glyph;
+    private Graphics2D g2d;
 
-    public FlattenedGlyphView(Glyph glyph) {
+    public ThicknessAlongPathVisualization(Glyph glyph) {
         this.glyph = glyph;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
+        g2d = (Graphics2D) g;
         g2d.scale(1, -1);
         g2d.translate(0, -getHeight());
 
+        drawGlyph(glyph);
+        drawThicknessLines(glyph);
+    }
+
+    private void drawGlyph(Glyph glyph) {
+        System.out.println("GLYPH");
         List<Contour> contours = glyph.getContours();
         for (Contour contour : contours) {
-            List<ch.fhnw.ip5.digitalfontclassification.domain.Point> outlinePoints = contour.getOutlinePoints();
+            java.util.List<Point> outlinePoints = contour.getOutlinePoints();
 
             // Draw segments connecting outline points
             g2d.setColor(Color.BLACK);
             List<Segment> segments = contour.getSegments();
             for (Segment segment : segments) {
-                ch.fhnw.ip5.digitalfontclassification.domain.Point from = segment.getFrom();
+                Point from = segment.getFrom();
                 Point to = segment.getTo();
                 g2d.drawLine((int) from.getX(), (int) from.getY(), (int) to.getX(), (int) to.getY());
             }
+        }
+    }
+
+    private void drawThicknessLines(Glyph glyph) {
+        List<Line> lines = LineThicknessAnalyzer.computeThicknessLinesAlongPathAtMiddleOfSegments(glyph);
+        System.out.println("HELLO");
+        g2d.setColor(Color.RED);
+        for(Line line : lines) {
+            Point from = line.getFrom();
+            Point to = line.getTo();
+            g2d.drawLine((int) from.getX(), (int) from.getY(), (int) to.getX(), (int) to.getY());
         }
     }
 
@@ -50,19 +68,10 @@ public class FlattenedGlyphView extends JPanel {
         Flattener flattener = new JavaAwtFlattener(flatness);
         Glyph flattenedGlyph = flattener.flatten(glyph);
 
-        System.out.println(
-                flattenedGlyph.getContours().stream()
-                .mapToInt(contour -> contour.getSegments().size())
-                .sum()
-        );
-
-        List<Double> thicknesses = computeThicknessAlongPathAtMiddleOfSegments(flattenedGlyph);
-        System.out.println(thicknesses);
-
         JFrame frame = new JFrame();
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new FlattenedGlyphView(flattenedGlyph));
+        frame.getContentPane().add(new ThicknessAlongPathVisualization(flattenedGlyph));
         frame.setVisible(true);
     }
 }
