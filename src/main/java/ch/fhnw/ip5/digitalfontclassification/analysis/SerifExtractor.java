94fcfe6d489415a5b197c6f5f7127f3ac3a7ae81
Joel Wälti <joel.t.waelti@hotmail.com>
2024-06-18 10:21:34 +0200

    remove old test code

--- src/main/java/ch/fhnw/ip5/digitalfontclassification/analysis/SerifExtractor.java
+++ src/main/java/ch/fhnw/ip5/digitalfontclassification/analysis/SerifExtractor.java
@@ -16,36 +16,6 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class SerifExtractor {
-    public static void main(String[] args) throws IOException, FontParserException {
-        String fontPath = args[0];
-        String targetPath = args[1];
-        char character = args[2].charAt(0);
-        int unitsPerEm = Integer.parseInt(args[3]);
-        double flatness = Double.parseDouble(args[4]);
-
-        FontParser parser = new JavaAwtFontParser(fontPath);
-        Glyph glyph = parser.getGlyph(character, unitsPerEm);
-        Flattener flattener = new JavaAwtFlattener(flatness);
-        Glyph flattenedGlyph = flattener.flatten(glyph);
-
-        BufferedImage bi = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = bi.createGraphics();
-        DrawUtil.setOriginToBottomLeft(g2d, bi.getHeight(), 100);
-
-        Supplier<Color> colorSupplier = () -> Color.RED;
-        DrawUtil.drawLines(getSerifLines(flattenedGlyph), g2d, colorSupplier);
-
-        Path plotFilePath = Path.of(targetPath, "serif.png");
-        File plotFile = plotFilePath.toFile();
-        if (!Files.exists(plotFilePath.getParent())) {
-            Files.createDirectories(plotFilePath.getParent());
-        }
-        System.out.println(plotFilePath);
-        ImageIO.write(bi, "PNG", plotFile);
-    }
-
-    private static final double THRESHOLD = 0.2;
-
     private final Glyph glyph;
     private final double serifHeightThreshold;
     private double[] directions;
